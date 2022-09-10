cfa6e54909075a34b94a70eeab27543c324ac2c2
Habeeb Alabi <habeeb.olamighty22@gmail.com>
2022-09-10 23:11:10 +0000

    Variables if else and while statements tasks 0-9

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,11 +5,11 @@
  * Return: alway returns 0
  */
 int main(void)
-{
+
 	int a = 0;
 
-	for(; a < 10 ; a++)
+	for (; a < 10 ; a++)
 		putchar('0' + a);
-	putchar('\n');
-	return(0);
+	putchar ('\n');
+	return (0);
 }
