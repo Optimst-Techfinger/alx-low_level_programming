ffe5eca0dcef9b4140730f04c888764162b7d37e
Habeeb Alabi <habeeb.olamighty22@gmail.com>
2022-09-11 08:54:48 +0000

    Variables if else and while statements tasks 0-9

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,18 +1,13 @@
-Task 0
-
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
 /**
- *  main - assign a random number to the variable n each time it is executed
- *  if the number is greater than 0: is positive, if the number is 0: is zero
- *  if the number is less than 0: is negative, followed by a new line
- *  Return: 0
+ * main - Entry point
+ *
+ * Return: return one of the 3 options
  */
-
-int main(void)
+	int main(void)
 {
 	int n;
 
@@ -20,15 +15,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positive\n", n);
+	printf("%d is positive\n", n);
 	}
-	else if (n < 0)
+	else if (n == 0)
 	{
-		printf("%d is negative\n", n);
+	printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("%d is zero\n", n);
+	printf("%d is negative\n", n);
 	}
 	return (0);
 }
