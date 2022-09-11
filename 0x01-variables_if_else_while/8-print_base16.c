73d869efcff1450ef532e3ede38bc447aa69858a
Habeeb Alabi <habeeb.olamighty22@gmail.com>
2022-09-11 09:40:43 +0000

    Variables if else and while statements tasks 0-9

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,26 +1,26 @@
-#include <stdio.h>
 
+#include <stdio.h>
 /**
  * main - entry point
  *
  * Return: alway returns 0
  * 
  */
-	int main(void)
+int main(void)
 {
 	unsigned char a = '0';
-	int i;
+	int i
 
 	for (i = 0; i < 10; i++)
 	{
-	putchar(a);
-	a++;
+		putchar(a);
+		a++;
 	}
 	a = '1';
 	for (i = 0; i < 6; i++)
 	{
-	putchar('0' + a);
-	a++;
+		putchar('0' + a);
+		a++;
 	}
 	putchar('\n');
 	return (0);
