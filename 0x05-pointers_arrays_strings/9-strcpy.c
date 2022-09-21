112a5c80ba34b02994408d3b2ba196ed7a848b5d
Habeeb Alabi <habeeb.olamighty22@gmail.com>
2022-09-21 12:15:38 +0000

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -13,7 +13,7 @@ char *_strcpy(char *dest, char *src)
 	int srcLength = 0;
 
 	while (dest[i] != '\0')
-		dest[i++] = '\0');
+		dest[i++] = '\0';
 	i = 0;
 	while (src[srcLength] != '\0')
 		srcLength++;
