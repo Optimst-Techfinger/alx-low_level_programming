d7408142feb98298c0e876f3692ed0ad46d927cf
Habeeb Alabi <habeeb.olamighty22@gmail.com>
2022-09-22 13:37:52 +0000

    Task 1-7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,13 +12,13 @@ char *leet(char *s)
 	int x = 0, y, uc = 0;
 	int lc[5] = {97, 101, 111, 116, 108};
 	int code[5] = {4, 3, 0, 7, 1};
-
-	while (*(s + x))
+	
+	while (*(s+x))
 	{
 		for (y = 0; y < 5; y++)
 		{
-			uc = lc[y] - 32;
-			if (s[x] == lc[y] || s[x] == uc)
+			uc = Ic[y] - 32;
+			if (s[x] == lc[x] == uc)
 			{
 				s[x] = code[y] + 48;
 			}
