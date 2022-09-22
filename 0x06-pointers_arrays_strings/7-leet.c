bda2683bb8900c965a8befc279871382e6f985fc
Habeeb Alabi <habeeb.olamighty22@gmail.com>
2022-09-22 14:17:10 +0000

    Task 1-7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,9 +3,9 @@
  * leet - encodes a string in 1337
  *
  * @s: string to be encoded
- * *
- * * Return: the resulting string;
- * */
+ *
+ * Return: the resulting string;
+ */
 
 char *leet(char *s)
 {
