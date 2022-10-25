9f348581d6edce2de974ddffdf8e22748e32014f
Habeeb Alabi <habeeb.olamighty22@gmail.com>
2022-10-25 22:39:10 +0000

    inital

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -20,7 +20,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		if (new->next == NULL)
 			return (NULL);
+		new = new->next;
 	}
-			new = new->next;
 	return (new);
 }
